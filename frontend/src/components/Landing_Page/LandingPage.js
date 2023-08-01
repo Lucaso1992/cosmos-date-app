24dc86abd0558c072f154f5b0bd7368291ace451
lucas solari <solarilucas92@gmail.com>
2023-08-01 20:55:12 +0200

    add landing-view & match-vieeeeew

--- frontend/src/components/Landing_Page/LandingPage.js
+++ frontend/src/components/Landing_Page/LandingPage.js
@@ -4,7 +4,7 @@ export const LandingPage = () => {
   return (
 <div className={style.landing_container}>
       <div className={style.landing_div}>
-        <h1 className="mb-4"><strong>Welcome to Cosmos</strong></h1>
+        <h1 className="mb-4"><strong>Welcome to Cosmos!</strong></h1>
         <p className="card-text"><strong>
 Enter a world where the magic of astrology merges with the thrill of arranged dating, offering you a unique experience in love. We are delighted to welcome you to Cosmos, where the planets are our allies in connecting like-minded hearts.</strong></p>
         <div>
