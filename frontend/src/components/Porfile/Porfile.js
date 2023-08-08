b88f2bbf699ebdea29e3b55e85b7ca7ffb86e1b4
Dámaso Armas <127236212+dasktorm@users.noreply.github.com>
2023-08-08 12:50:33 +0000

    account details and home view ver. 1.1

--- frontend/src/components/Porfile/Porfile.js
+++ frontend/src/components/Porfile/Porfile.js
@@ -4,13 +4,13 @@ export const Porfile = () => {
   return (
     <div className={style.profile_container}>
       <div className={style.main_header}>
-        <p>Profile</p>
+        <h1 className="p-1 ms-3">Profile</h1>
       </div>
 
       <div className={style.main_layout}>
         <div className={style.personal_info}>
-          <h1>Personal Info</h1>
-          <button>Edit</button>
+          <h4 className="p-0 mb-0">Personal Info</h4>
+          <button className="btn p-0 mt-0 mb-2">Edit</button>
           <form>
             <div class="input-group my-2">
               <label for="form-name" class="form-label"></label>
@@ -33,14 +33,20 @@ export const Porfile = () => {
             </div>
 
             <div className="input-group my-2">
-              <select
-                class="form-select"
-                aria-label="Default select example"
-              >
+              <select class="form-select" aria-label="Default select example">
                 <option selected>Zodiac Sign</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <option value="1">Aries</option>
+                <option value="2">Taurus</option>
+                <option value="3">Gemini</option>
+                <option value="4">Cancer</option>
+                <option value="5">Leo</option>
+                <option value="6">Virgo</option>
+                <option value="7">Libra</option>
+                <option value="8">Scorpius</option>
+                <option value="9">Sagittarius</option>
+                <option value="10">Capricornus</option>
+                <option value="11">Aquarius</option>
+                <option value="12">Pices</option>
               </select>
             </div>
 
@@ -55,22 +61,18 @@ export const Porfile = () => {
             </div>
 
             <div className="input-group my-2">
-              <select
-                class="form-select"
-                aria-label="Default select example"
-              >
+              <select class="form-select" aria-label="Default select example">
                 <option selected>Country</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+                <option value="1">Argentina</option>
+                <option value="2">Spain</option>
+                <option value="2">Guatemala</option>
+                <option value="4">Venezuela</option>
+                <option value="5">Other</option>
               </select>
             </div>
-          
+
             <div className="input-group my-2">
-              <select
-                class="form-select"
-                aria-label="Default select example"
-              >
+              <select class="form-select" aria-label="Default select example">
                 <option selected>Gender</option>
                 <option value="1">Female</option>
                 <option value="2">Male</option>
@@ -79,18 +81,17 @@ export const Porfile = () => {
             </div>
 
             <input type="date" id="start" name="birthdate" />
-
           </form>
         </div>
         <div className={style.about_photos}>
           <div className={style.photos}>
-            <h1>Photos</h1>
+            <h4 className="p-3">Photos</h4>
           </div>
           <div className={style.about}>
-            <h1>About You</h1>
+            <h4 className="p-3">About Me</h4>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
