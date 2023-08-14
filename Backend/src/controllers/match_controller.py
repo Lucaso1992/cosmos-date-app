ecfb4f1c00e4e3a41cb463ceebdcc028213a8a01
Geovanny Valladares <geovannyvalladares94@gmail.com>
2023-08-14 16:27:01 +0200

    hotfix

--- Backend/src/controllers/match_controller.py
+++ Backend/src/controllers/match_controller.py
@@ -5,13 +5,24 @@ from sqlalchemy import not_
 
 from utils.db import db
 from models.users import User
+from models.profile_likes import Profile_Like
+from models.profile_dislikes import Profile_Dislike
 
 def get_match(user):
     user_db = User.query.get(user["id"])
-    posible_match_db = User.query.filter(User.id != user["id"]).\
-        filter(not_(User.id.in_(user_db.dislikes_to))).\
-        filter(not_(User.id.in_(user_db.likes_to))).first()
+    # posible_match_db = User.query.filter(User.id != user["id"]).\
+    #     filter(not_(User.id.in_(user_db.dislikes_to))).\
+    #     filter(not_(User.id.in_(user_db.likes_to))).first()
+
+    # posible_match_db = User.query.filter(User.id != user_db.id).\
+    #     filter(not_(Profile_Dislike.user_to_id == User.id)). \
+    #     filter(not_(Profile_Like.user_to_id == User.id)).first()
+
+    posible_match_db = User.query.filter(User.id != user_db.id).\
+        filter(~User.dislikes_to.any(User.id == user["id"])).\
+        filter(~User.likes_to.any(User.id == user["id"])).first()
     
+
     if posible_match_db is None:
         return jsonify({"message": "Match not found"}), 400
     else:
@@ -23,4 +34,4 @@ def get_match(user):
             )
         posible_match["profile"]["age"]=diference.years
 
-        return posible_match
+        return jsonify(posible_match), 200
