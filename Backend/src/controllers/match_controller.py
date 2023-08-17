e23916b598b05da10767a6d521df36193a3b42a8
Geovanny Valladares <geovannyvalladares94@gmail.com>
2023-08-17 17:36:44 +0200

    hot fix

--- Backend/src/controllers/match_controller.py
+++ Backend/src/controllers/match_controller.py
@@ -5,19 +5,29 @@ from sqlalchemy import not_
 
 from utils.db import db
 from models.users import User
-from models.profile_likes import Profile_Like
-from models.profile_dislikes import Profile_Dislike
+from models.profile_info import Profile
+# from models.profile_dislikes import Profile_Dislike
 
 def get_match(user):
     user_db = User.query.get(user["id"])
 
     dislike_list = [User.id for User in user_db.dislikes_to]
     like_list = [User.id for User in user_db.likes_to]
-    query = db.select(User).filter(
-            User.id != user_db.id,
-            User.id.not_in(dislike_list),
-            User.id.not_in(like_list)
-        )
+
+    if user_db.profile.love_interest=="Indifferent":
+        query = db.select(User).filter(
+                User.id != user_db.id,
+                User.id.not_in(dislike_list),
+                User.id.not_in(like_list),
+            )
+    if user_db.profile.love_interest!="Indifferent":
+        query = db.select(User).filter(
+                User.id != user_db.id,
+                User.id.not_in(dislike_list),
+                User.id.not_in(like_list),
+                Profile.user_id == User.id,
+                Profile.gender == user_db.profile.love_interest
+            )
 
     posible_match_db = db.session.execute(query).scalars().first()
     
