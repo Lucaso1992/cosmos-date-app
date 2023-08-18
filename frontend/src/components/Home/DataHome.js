ddaf424c682c0b674c49d1a4ac08a4c6cf075a84
lucas solari <solarilucas92@gmail.com>
2023-08-18 17:22:15 +0200

    home View

---
+++ frontend/src/components/Home/DataHome.js
@@ -0,0 +1,82 @@
+export const DataHome = [
+    {
+        horoscope: 'Aries',
+        zodical_sign: '♈︎',
+        text: 'Aries is a passionate and energetic fire sign. Natural leaders, brave, and adventurous.'
+    },
+    {
+        horoscope: 'Taurus',
+        zodical_sign: '♉︎',
+        text: 'Taurus is an earth sign valuing stability and sensuality. Persistent and fond of luxury.'
+    },
+    {
+        horoscope: 'Gemini',
+        zodical_sign: '♊︎',
+        text: 'Gemini is an adaptable and curious air sign. Communicative, intelligent, and versatile in interests.'
+    },
+    {
+        horoscope: 'Cancer',
+        zodical_sign: '♋︎',
+        text: 'Cancer is an emotional and protective water sign. Intuitive, loyal, and deeply tied to family.'
+    },
+    {
+        horoscope: 'Leo',
+        zodical_sign: '♌︎',
+        text: 'Leo is a magnetic and self-assured fire sign. Creative, generous, and loves the spotlight.'
+    },
+    {
+        horoscope: 'Virgo',
+        zodical_sign: '♍︎',
+        text: 'Virgo is a perfectionist and analytical earth sign. Practical, detail-oriented, and strives for excellence.'
+    },
+    {
+        horoscope: 'Libra',
+        zodical_sign: '♎︎',
+        text: 'Libra is a peace-loving and balanced air sign. Sociable, charming, and seeks harmony in everything.'
+    },
+    {
+        horoscope: 'Scorpio',
+        zodical_sign: '♏︎',
+        text: 'Scorpio is a passionate and enigmatic water sign. Intense, determined, and possesses deep emotional depth.'
+    },
+    {
+        horoscope: 'Sagittarius',
+        zodical_sign: '♐︎',
+        text: 'Sagittarius is an adventurous and optimistic fire sign. Philosophical, independent, and constantly seeking truth.'
+    },
+    {
+        horoscope: 'Capricorn',
+        zodical_sign: '♑︎',
+        text: 'Capricorn is an ambitious and disciplined earth sign. Responsible, practical, and strives for success.'
+    },
+    {
+        horoscope: 'Aquarius',
+        zodical_sign: '♒︎',
+        text: 'Aquarius is an innovative and humanitarian air sign. Original, independent, and dedicated to social causes.'
+    },
+    {
+        horoscope: 'Pisces',
+        zodical_sign: '♓︎',
+        text: 'Pisces is an empathetic and dreamy water sign. Creative, intuitive, and deeply connected to emotions.'
+    }
+];
+
+
+
+
+
+
+
+
+// Aries: "♈︎",
+// Taurus: "♉︎",
+// Gemini: "♊︎",
+// Cancer: "♋︎",
+// Leo:    "♌︎",
+// Virgo: "♍︎",
+// Libra: "♎︎",
+// Scorpio: "♏︎",
+// Sagittarius: "♐︎",
+// Capricorn: "♑︎",
+// Aquarius: "♒︎",
+// Pisces: "♓︎"
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,24 +1,16 @@
 import style from "./Home.module.css";
+import { DataHome } from "./DataHome";
+import { HomeCards } from "./HomeCards";
+
 
 export const Home = () => {
   return (
     <div className={style.main_layout}>
-      <h2 className='text-center my-3'>
+      <h2 className={style.horoscope_title}>
         Horoscopes
       </h2>
       <div className={style.cards_father}>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
-        <div className={style.horoscope_cards}></div>
+      {DataHome.map(item => <HomeCards horoscope={item.horoscope} zodical_sign={item.zodical_sign} text={item.text}/>)}
       </div>
     </div>
   );
